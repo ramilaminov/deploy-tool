6825ea78297038cf3865efd0fb2862aa4eabf021
Ramil Aminov <ramil@aminov.dev>
2024-04-23 18:43:05 +0300

    Constructing compose files from TS objects

--- templates/service-compose.ts
+++ templates/service-compose.ts
@@ -1,3 +1,5 @@
+import YAML from "yaml";
+
 export const serviceCompose = ({
   name,
   image,
@@ -12,47 +14,46 @@ export const serviceCompose = ({
   domain: string;
   path?: string;
   environment: string[];
-}) => `
-version: '3.8'
-
-services:
-  ${name}:
-    image: ${image}
-    ${buildEnvironment(environment)}
-    deploy:
-      replicas: 1
-      update_config:
-        parallelism: 1
-        failure_action: rollback
-        delay: 10s
-        order: start-first
-      rollback_config:
-        parallelism: 0
-        order: stop-first
-      restart_policy:
-        condition: any
-        delay: 5s
-        max_attempts: 3
-        window: 120s
-      labels:
-        - traefik.enable=true
-        - traefik.http.services.${name}.loadbalancer.server.port=${port}
-        - traefik.http.routers.${name}.rule=Host(\`${domain}\`)${
-          path !== "/" ? ` && PathPrefix(\`${path}\`)` : ""
-        }
-        - traefik.http.routers.${name}.entrypoints=websecure
-    networks:
-      - traefik-public
-
-networks:
-  traefik-public:
-    external: true
-`;
-
-const buildEnvironment = (variables: string[]): string => {
-  if (variables.length === 0) return "";
-  // TODO Check variable format (name=value) and escape special characters
-  return `environment:\n${variables
-    .map((variable) => `      - ${variable}`)
-    .join("\n")}`;
-};
+}) =>
+  YAML.stringify({
+    version: "3.8",
+    services: {
+      [name]: {
+        image: image,
+        environment: environment.length > 0 ? environment : undefined,
+        deploy: {
+          replicas: 1,
+          update_config: {
+            parallelism: 1,
+            failure_action: "rollback",
+            delay: "10s",
+            order: "start-first",
+          },
+          rollback_config: {
+            parallelism: 0,
+            order: "stop-first",
+          },
+          restart_policy: {
+            condition: "any",
+            delay: "5s",
+            max_attempts: 3,
+            window: "120s",
+          },
+          labels: {
+            "traefik.enable": true,
+            [`traefik.http.services.${name}.loadbalancer.server.port`]: port,
+            [`traefik.http.routers.${name}.rule`]: `Host(\`${domain}\`)${
+              path !== "/" ? ` && PathPrefix(\`${path}\`)` : ""
+            }`,
+            [`traefik.http.routers.${name}.entrypoints`]: "websecure",
+          },
+        },
+        networks: ["traefik-public"],
+      },
+    },
+    networks: {
+      "traefik-public": {
+        external: true,
+      },
+    },
+  });
--- templates/traefik-compose.ts
+++ templates/traefik-compose.ts
@@ -1,43 +1,49 @@
-export const traefikCompose = () => `
-version: '3.8'
+import YAML from "yaml";
 
-services:
-  traefik:
-    image: traefik:v3.0
-    command:
-      - --log
-      - --log.level=DEBUG # only for debug
-      - --accesslog # only for debug
-      - --providers.docker=true
-      - --providers.swarm.endpoint=unix:///var/run/docker.sock
-      - --providers.docker.exposedbydefault=false
-      - --entrypoints.web.address=:80
-      - --entrypoints.web.http.redirections.entryPoint.to=websecure
-      - --entrypoints.web.http.redirections.entryPoint.scheme=https
-      - --entrypoints.websecure.address=:443
-      - --entrypoints.websecure.http.tls.certResolver=letsencrypt
-      - --certificatesresolvers.letsencrypt.acme.storage=/certificates/acme.json
-      - --certificatesresolvers.letsencrypt.acme.tlschallenge=true
-    ports:
-      - 80:80
-      - 443:443
-    volumes:
-      - /var/run/docker.sock:/var/run/docker.sock:ro
-      - traefik-certificates:/certificates
-    deploy:
-      placement:
-        constraints:
-          - node.role == manager
-      labels:
-        - traefik.enable=false
-        - traefik.docker.network=traefik-public
-    networks:
-      - traefik-public
-
-volumes:
-  traefik-certificates:
-
-networks:
-  traefik-public:
-    external: true
-`;
+export const traefikCompose = () =>
+  YAML.stringify({
+    version: "3.8",
+    services: {
+      traefik: {
+        image: "traefik:v3.0",
+        command: [
+          "--log",
+          "--log.level=DEBUG", // Only for debug
+          "--accesslog", // Only for debug
+          "--providers.docker=true",
+          "--providers.swarm.endpoint=unix:///var/run/docker.sock",
+          "--providers.docker.exposedbydefault=false",
+          "--entrypoints.web.address=:80",
+          "--entrypoints.web.http.redirections.entryPoint.to=websecure",
+          "--entrypoints.web.http.redirections.entryPoint.scheme=https",
+          "--entrypoints.websecure.address=:443",
+          "--entrypoints.websecure.http.tls.certResolver=letsencrypt",
+          "--certificatesresolvers.letsencrypt.acme.storage=/certificates/acme.json",
+          "--certificatesresolvers.letsencrypt.acme.tlschallenge=true",
+        ],
+        ports: ["80:80", "443:443"],
+        volumes: [
+          "/var/run/docker.sock:/var/run/docker.sock:ro",
+          "traefik-certificates:/certificates",
+        ],
+        deploy: {
+          placement: {
+            constraints: ["node.role == manager"],
+          },
+          labels: {
+            "traefik.enable": false,
+            "traefik.docker.network": "traefik-public",
+          },
+        },
+        networks: ["traefik-public"],
+      },
+    },
+    volumes: {
+      "traefik-certificates": {},
+    },
+    networks: {
+      "traefik-public": {
+        external: true,
+      },
+    },
+  });
